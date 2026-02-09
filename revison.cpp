8f892f4e122f857ba38c44815d228f4e5cb547af
ritikayadav01 <144210251+ritikayadav01@users.noreply.github.com>
2026-02-09 11:29:01 +0530

    cow_chiken_problem

--- revison.cpp
+++ revison.cpp
@@ -1,60 +1,23 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main()
-{
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int t;
-    cin>>t;
-    while(t--)
-    {
-        int n ;
-        cin>>n;
-        vector<int>arr(n);
-        for(int i=0;i<n;i++)cin>>arr[i];
-        int ans =0;
-        if(arr[0]==-1 && arr[n-1]==-1)
-        {
-            for(int i=0;i<n;i++)
-            {
-                if(arr[i]==-1)arr[i]=0;
-            }
-            cout<<0<<endl;
-            for(int i=0;i<n;i++)cout<<arr[i]<<" ";
-            cout<<endl;
-        }
-        else if(arr[0]==-1)
-        {
-            arr[0]=arr[n-1];
-            for(int i=0;i<n;i++)
-            {
-                if(arr[i]==-1)arr[i]=0;
-            }
-            cout<<0<<endl;
-            for(int i=0;i<n;i++)cout<<arr[i]<<" ";
-            cout<<endl;
-        }
-        else if(arr[n-1]==-1)
-        {
-            arr[n-1]=arr[0];
-            for(int i=0;i<n;i++)
-            {
-                if(arr[i]==-1)arr[i]=0;
-            }
-            cout<<0<<endl;
-            for(int i=0;i<n;i++)cout<<arr[i]<<" ";
-            cout<<endl;
-        }
-        else 
-        {
-            for(int i=0;i<n;i++)
-            {
-                if(arr[i]==-1)arr[i]=0;
-            }
-            cout<<abs(arr[0]-arr[n-1])<<endl;
-            for(int i=0;i<n;i++)cout<<arr[i]<<" ";
-            cout<<endl;
-        }
+    cin >> t;
 
+    while (t--) {
+        int n;
+        cin >> n;
 
-
+        if (n % 2 != 0) {
+            cout << 0 << "\n";
+        } else {
+            cout << (n / 4) + 1 << "\n";
+        }
     }
+
+    return 0;
 }
