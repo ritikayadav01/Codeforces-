60adfdde07afe184eaa24ead48111367def59c02
ritikayadav01 <144210251+ritikayadav01@users.noreply.github.com>
2026-06-06 12:56:29 +0530

    construct array -800

---
+++ 2231A.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n ;
+        cin>>n;
+        vector<int>ans(n);
+       for(int i=0;i<n;i++)
+    cout << 2*i+1 << " ";
+cout << '\n';
+}
+}
